4191d0d2e3eaef17948aad968daaaba1cd78b815
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-08 22:16:27 -0300

    copy gatsby-node to gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react"
+import Layout from "./src/templates/Layout.jsx"
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => <Layout>{element}</Layout>

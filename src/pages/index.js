299d04d327a4517194a516915e7cb4b6f0137717
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-08 21:54:59 -0300

    remove login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import * as React from "react"
 import { LoginForm } from "../components"
 
-const Index = () => (
-  <>
-    <LoginForm />
-  </>
-)
+const Index = () => <>{/* <LoginForm /> */}</>
 
 export default Index

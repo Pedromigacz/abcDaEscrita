9235488c3be633b2fb9a5236411f1f1db7079c06
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-09 15:34:31 -0300

    extract AppBar to its own component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as LoginForm } from "./LoginForm.jsx"
 export { default as Copyright } from "./Copyright.jsx"
 export { default as Drawer } from "./Drawer.jsx"
+export { default as AppBar } from "./AppBar.jsx"
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,39 +1,15 @@
 import React, { useState } from "react"
 import { Copyright } from "../../components"
-import { Drawer } from "../../components"
+import { Drawer, AppBar } from "../../components"
 
 // materiaul ui imports
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import Box from "@mui/material/Box"
-import MuiAppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
-import IconButton from "@mui/material/IconButton"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
-import MenuIcon from "@mui/icons-material/Menu"
-
-const drawerWidth = 240
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: prop => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}))
 
 const mdTheme = createTheme()
 
@@ -47,40 +23,8 @@ function DashboardContent() {
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: "24px", // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              ABC da escrita
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          open={open}
-          drawerWidth={drawerWidth}
-          toggleDrawer={toggleDrawer}
-        />
+        <AppBar open={open} toggleDrawer={toggleDrawer} />
+        <Drawer open={open} toggleDrawer={toggleDrawer} />
         <Box
           component="main"
           sx={{

929c26958d180386ca12e71ac532a6d538253b4f
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-08 21:25:18 -0300

    remove not filled papers

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -139,31 +139,9 @@ function DashboardContent() {
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                ></Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                ></Paper>
-              </Grid>
-              {/* Recent Orders */}
               <Grid item xs={12}>
                 <Paper
-                  sx={{ p: 2, display: "flex", flexDirection: "column" }}
+                  sx={{ p: 10, display: "flex", flexDirection: "column" }}
                 ></Paper>
               </Grid>
             </Grid>

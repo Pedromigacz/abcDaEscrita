2f0875a9f09436ef923f734093db29d0285d4986
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-23 19:10:23 -0300

    change == to ===

--- src/components/UserCourseList.jsx
+++ src/components/UserCourseList.jsx
@@ -15,9 +15,9 @@ import { Link } from "gatsby"
 function dataAtualFormatada() {
   var data = new Date(),
     dia = data.getDate().toString(),
-    diaF = dia.length == 1 ? "0" + dia : dia,
+    diaF = dia.length === 1 ? "0" + dia : dia,
     mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-    mesF = mes.length == 1 ? "0" + mes : mes,
+    mesF = mes.length === 1 ? "0" + mes : mes,
     anoF = data.getFullYear()
   return diaF + "/" + mesF + "/" + anoF
 }

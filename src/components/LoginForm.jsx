bd324be16f8dc81a24ef10b3f8c83106793d2e27
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-26 18:50:08 -0300

    clean loginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,7 +3,6 @@ import { FirebaseContext } from "../contexts/firebaseContext.js"
 import { Copyright, ResetPasswordModal } from "../components"
 
 // Mui imports
-import Avatar from "@mui/material/Avatar"
 import LoadingButton from "@mui/lab/LoadingButton"
 import CssBaseline from "@mui/material/CssBaseline"
 import TextField from "@mui/material/TextField"
@@ -11,8 +10,6 @@ import Link from "@mui/material/Link"
 import Paper from "@mui/material/Paper"
 import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
-import Typography from "@mui/material/Typography"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import BlueVerticalLogo from "../vectors/BlueVerticalLogo.jsx"
 

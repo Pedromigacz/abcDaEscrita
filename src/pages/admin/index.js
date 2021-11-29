436f909b0ab5956045a5b615fa52fed5ae3fd724
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-11-29 10:28:51 -0300

    add index page to admin

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,7 +2,12 @@ import React from "react"
 import AdminTemplate from "../../templates/AdminTemplate.jsx"
 
 const Index = () => {
-  return <AdminTemplate></AdminTemplate>
+  return (
+    <AdminTemplate>
+      <h1>Olá, seja bem vindo ao painel de administração</h1>
+      <p>Aqui você consegue gerenciar seus cursos, aulas e usuários.</p>
+    </AdminTemplate>
+  )
 }
 
 export default Index
